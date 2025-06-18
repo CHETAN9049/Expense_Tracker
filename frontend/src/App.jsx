bcaf564a699fe2dae2aad1824ffed7c56c71e040
tiya-sur <tiyasur28@gmail.com>
2025-06-19 02:33:42 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import Login from './pages/Login'; 
-
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import HomePage from './pages/HomePage';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div>
-      <Login />
-  
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
 export default App;
+

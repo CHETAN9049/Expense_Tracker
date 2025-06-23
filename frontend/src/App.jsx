198022ba221e9c604772ddfb43daf5b72008a044
tiya-sur <tiyasur28@gmail.com>
2025-06-23 17:29:58 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,20 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import HomePage from './pages/HomePage';
 import Dashboard from './components/Dashboard';
+import AdminLogin from './pages/AdminLogin';
+import AdminDashboard from './pages/AdminDashboard';
+import ViewUsers from './pages/ViewUsers';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/" element={<HomePage />} />
+      <Route path="/adminlogin" element={<AdminLogin />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/addashboard" element={<AdminDashboard />} />
+      <Route path="/view-users" element={<ViewUsers />} />
     </Routes>
   );
 }

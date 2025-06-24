4874582dcba8772a7fed8d42282aac4cebef0103
tiya-sur <tiyasur28@gmail.com>
2025-06-24 17:35:58 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import HomePage from './pages/HomePage';
+import ProfilePage from './components/ProfilePage.jsx';
 import Dashboard from './components/Dashboard';
+import HistoryPage from './components/HistoryPage.jsx';
 import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
 import ViewUsers from './pages/ViewUsers';
 import Verify2FA from './pages/Verify2FA';
+
 
 function App() {
   return (
@@ -16,7 +19,9 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/adminlogin" element={<AdminLogin />} />
+      <Route path='/profilepage' element={<ProfilePage/>}/>
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path='/history' element={<HistoryPage/>}/>
       <Route path="/addashboard" element={<AdminDashboard />} />
       <Route path="/view-users" element={<ViewUsers />} />
       <Route path="/verify-2fa" element={<Verify2FA/>}/>

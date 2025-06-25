165958c4fc6f693f9c5e88807bb04f3a8655f9fd
JellyScript2 <161505720+JellyScript2@users.noreply.github.com>
2025-06-25 08:45:07 +0530

    Update NavigationBar.jsx

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const NavigationBar = () => {
+const NavigationBar = () => 
+  {
   const navigate = useNavigate()
 
   const styles = {
